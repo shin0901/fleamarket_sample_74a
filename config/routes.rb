49417ab8bacfd0ca0979704b35d930be7b7f5b2f
ayahonda <frede5ri@yahoo.co.jp>
2020-07-31 03:56:41 +0900

    フォルダの移動
    sell.html.hamlとshow.html.hamlをproductsフォルダに移動
    productsコントローラのshowアクションに@searchを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'products/index'
   get 'searches/index'
   root 'top#index'
-  resources :top
+  resources :top, only: [:index, :new]
+  resources :products, only: [:index, :new, :show]
 end
