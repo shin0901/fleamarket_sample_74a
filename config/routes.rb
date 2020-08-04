611f754542acd591173557952078aa0414b395f2
higata-a <64731787+higata-a@users.noreply.github.com>
2020-08-04 21:07:45 +0900

    1
    usersフォルダ、新規ユーザー登録、お客様情報、支払い情報ページを作成
    users_controller、users.scss作成
    アプリケーションコントローラーにusers.scssを登録

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'products/index'
   get 'searches/index'
   root 'top#index'
+  # root 'users#payment'
   resources :top, only: [:index, :new]
   resources :products, only: [:index, :new, :show]
+  resources :users, only: [:index, :new]
 
 end
