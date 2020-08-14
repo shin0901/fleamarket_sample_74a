985e97c64b7e5dbf373c80243ae06dd37fc995b3
ayahonda <frede5ri@yahoo.co.jp>
2020-08-14 13:15:07 +0900

    カテゴリーテーブルの作成
    string型のnameカラム（null:false）とstring型のancestryカラムをデフォルトで作成
    
    参考
    https://techtechmedia.com/gem-ancestry-rails/

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_112700) do
+ActiveRecord::Schema.define(version: 2020_08_14_040319) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image"
@@ -19,10 +26,12 @@ ActiveRecord::Schema.define(version: 2020_08_01_112700) do
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "produstName"
-    t.string "image"
+    t.string "productName", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["productName"], name: "index_products_on_productName"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
