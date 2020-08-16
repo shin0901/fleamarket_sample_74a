f5a3108cb0601bd60e1e44b3944e4bb92dac7dcc
shin0901 <tsunagu.promotion@gmail.com>
2020-08-16 12:57:42 +0900

    models fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable
          :recoverable, :rememberable, :validatable, :password_length
 
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, uniqueness: true, length: { minimun: 7 }
+  validates :password, presence: true, uniqueness: true, length: { minimum: 7 }
 end
