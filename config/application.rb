7c7de426022c37d49c5a262c4d6c63923d4efa80
ayahonda <frede5ri@yahoo.co.jp>
2020-08-23 16:18:19 +0900

    フラッシュメッセージとエラーメッセージ関係

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,14 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module FleamarketSample74a
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.load_defaults 5.2
     config .generators do |g|
