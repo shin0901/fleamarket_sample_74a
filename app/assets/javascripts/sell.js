3e7406d3f0531b5c98c7721a499dde6244e2b91c
ayahonda <frede5ri@yahoo.co.jp>
2020-08-23 16:19:29 +0900

    文字数カウントのためのjQuery追記

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -1,4 +1,5 @@
 $(function(){
+  // 価格を入力すると手数料と利益が表示
   $('#product_price').on('keyup', function(){
     let input = $(this).val();
     if (input >= 300 && input <= 9999999){
@@ -11,4 +12,10 @@ $(function(){
       $('#profit_feild').html('-');
     }
   })
+
+  // 商品の説明入力フォームの文字数カウント
+  $('#product_description').keyup(function(){
+    var count = $(this).val().length;
+    $('.show-counter').text(count);
+  });
 });
