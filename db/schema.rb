c5574db479b8e22e0372df70f6b55f573bd82ccf
higata-a <64731787+higata-a@users.noreply.github.com>
2020-08-26 17:19:43 +0900

    db push log
    カテゴリのデータファイル作成

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_08_25_091337) do
     t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
