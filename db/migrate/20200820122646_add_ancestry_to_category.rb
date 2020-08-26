c370ddb53c8148f5f0f5d830ee772731d6f3ce48
higata-a <64731787+higata-a@users.noreply.github.com>
2020-08-26 17:21:32 +0900

    rest
    変更反映

--- db/migrate/20200820122646_add_ancestry_to_category.rb
+++ db/migrate/20200820122646_add_ancestry_to_category.rb
@@ -1,7 +1,6 @@
 class AddAncestryToCategory < ActiveRecord::Migration[5.2]
   def change
-    # add_column :categories, :ancestry, :integer
-    # categoriesテーブルにancestryカラムが無い場合に使用
+    add_column :categories, :ancestry, :integer
     add_index :categories, :ancestry
   end
 end
