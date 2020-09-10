7c7f917a0b9034b5050bb277db908b206b59405a
ayahonda <frede5ri@yahoo.co.jp>
2020-09-10 11:51:45 +0900

    update完了画面を削除してnoticeを追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,34 +18,6 @@ class ProductsController < ApplicationController
     @product.images.build
   end
 
-  def destroy
-    product = Product.find(params[:id])
-    respond_to do |format|
-      if product.destroy
-        format.html{redirect_to root_path, notice:'商品を削除しました'}
-      else
-        format.html{render action: 'edit', notice:'商品の削除に失敗しました'}
-      end
-    end
-  end
-  
-  def show
-    @product = Product.find(params[:id])
-    @category_id = @product.category_id
-    @category_parent = Category.find(@category_id).parent.parent
-    @category_child = Category.find(@category_id).parent
-    @category_grandchild = Category.find(@category_id)
-  end
-
-  def edit
-    @product = Product.find(params[:id])
-    # @product.images.build
-    @category_id = @product.category_id
-    @category_parent = Category.find(@category_id).parent.parent
-    @category_child = Category.find(@category_id).parent
-    @category_grandchild = Category.find(@category_id)
-  end
-  
   def create
     @product = Product.new(product_params)
     respond_to do |format|
@@ -57,10 +29,47 @@ class ProductsController < ApplicationController
       end
     end
   end
+
+  def show
+    @product = Product.find(params[:id])
+    @category_id = @product.category_id
+    @category_parent = Category.find(@category_id).parent.parent
+    @category_child = Category.find(@category_id).parent
+    @category_grandchild = Category.find(@category_id)
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    respond_to do |format|
+      if product.destroy
+        format.html{redirect_to root_path, notice:'商品を削除しました'}
+      else
+        format.html{render action: 'edit', notice:'商品の削除に失敗しました'}
+      end
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    @category_id = @product.category_id
+    @category_parent = Category.find(@category_id).parent.parent
+    @category_child = Category.find(@category_id).parent
+    @category_grandchild = Category.find(@category_id)
+  end
   
   def update
     @product = Product.find(params[:id])
-    @product.update(product_params)
+    @category_id = @product.category_id
+    @category_parent = Category.find(@category_id).parent.parent
+    @category_child = Category.find(@category_id).parent
+    @category_grandchild = Category.find(@category_id)
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html{redirect_to root_path, notice:'商品情報を更新しました'}
+      else
+        format.html{render action: 'edit'}
+      end
+    end
   end
 
   private
@@ -70,7 +79,6 @@ class ProductsController < ApplicationController
     :prefecture_id, :days_until_shipping_id, :shipping_charge_id, :brand_id, images_attributes:[:image, :id, :_destroy])
     .merge(user_id: current_user.id)
   end
-
   
   def set_category
     @category_parent_array = Category.where(ancestry: nil)
